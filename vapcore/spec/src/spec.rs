4138cd24bf4854c1a717b2cabc91435712258a24
marlonhanks <marlon.hanks@kedia.co>
2021-03-07 22:45:56 -0500

    declare module vapcore_clique

--- vapcore/spec/src/spec.rs
+++ vapcore/spec/src/spec.rs
@@ -38,7 +38,7 @@ use authority_round::AuthorityRound;
 use basic_authority::BasicAuthority;
 use bytes::Bytes;
 use builtin::Builtin;
-use clique::Clique;
+use vapcore_clique::Clique;
 use enjen::Engine;
 use vapash_engine::Vapash;
 use vapory_types::{H256, Bloom, U256, Address};
